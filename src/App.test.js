5c402e579806441efbfacc6a54c870b3a2acc881
Chí Nguyễn Đắc <dacchi6761@gmail.com>
2021-02-03 14:05:00 +0700

    fire event

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,28 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', async () => {
-  render(<App />);
-  expect(screen.queryByText(/Signed in as/)).toBeNull();
-  screen.debug();
-  expect(await screen.findByText(/Signed in as/)).toBeInTheDocument();
-  screen.debug();
-});
+describe('Test App', () => {
+  test('renders learn react link', async () => {
+    render(<App />);
+    expect(screen.queryByText(/Signed in as/)).toBeNull();
+    screen.debug();
+    expect(await screen.findByText(/Signed in as/)).toBeInTheDocument();
+    screen.debug();
+  });
+
+  test('User enter JS', async () => {
+    render(<App />);
+
+    await screen.findByText(/Signed in as/);
+
+    expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
+
+    screen.debug();
+    fireEvent.change(screen.getByRole('textbox'), {
+      target: { value: 'JavaScript' }
+    });
+    screen.debug();
+
+    expect(screen.queryByText(/Searches for JavaScript/)).toBeInTheDocument();
+  })
+})
